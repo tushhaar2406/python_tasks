cc7c661240118bae0989e05f1d7aa18aab091d79
Tushar Tambe <tushar.tambe@alignedautomation.com>
2025-12-30 12:00:50 +0530

    Added Assignment_1 Task_1 and Task_2

--- Assignment_1_Task_2.py
+++ Assignment_1_Task_2.py
@@ -1,4 +1,13 @@
+"""
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+
+
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
 print("Hello"+" "+ first_name +" "+ last_name + "! Welcome to Python program.")
+
