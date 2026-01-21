baad56d678d0e80ba3ec4eb82f393dba9e76fd07
Tushar <tushar.tambe@alignedautomation.com>
2026-01-21 15:34:35 +0530

    Assignment 3 task 1 and task 2 added

---
+++ Assignement_3_Task_2.py
@@ -0,0 +1,23 @@
+"""
+Problem Statement: Write a Python program that:
+1.   Asks the user for a number as input.
+2.   Uses the math module to calculate the:
+o   Square root of the number
+o   Natural logarithm (log base e) of the number
+o   Sine of the number (in radians)
+3.   Displays the calculated results.
+"""
+
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Number: {num}")
+print(f"Square root: {square_root}")
+print(f"Natural logarithm: {natural_log}")
+print(f"Sine (in radians): {sine_value}")
+
---
+++ Assignment_3_Task_1.py
@@ -0,0 +1,15 @@
+"""
+Problem Statement: Write a Python program that:
+1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
+2.   Returns the calculated factorial.
+3.   Calls the function with a sample number and prints the output.
+"""
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is: {factorial(num)}")
